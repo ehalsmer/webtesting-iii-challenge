61582fcf22ebe757589e99ed926e45971f46266f
Eleasah Halsmer <eleasahjh@gmail.com>
2019-08-19 14:47:59 -0600

    Wrote snapshot tests for main components

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,11 @@
-// Test away!
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import Controls from './Controls';
+
+describe('<Controls/>', () => {
+    it('matches snapshot', () => {
+        const tree = renderer.create(<Controls />);
+        expect(tree.toJSON()).toMatchSnapshot();
+    })
+})
