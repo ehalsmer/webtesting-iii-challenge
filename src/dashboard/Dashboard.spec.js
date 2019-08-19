6a6b2264ec584afe287e77e0d56b0163e6d8fae1
Eleasah Halsmer <eleasahjh@gmail.com>
2019-08-19 16:14:47 -0600

    Testing text on buttons depending on closed and locked mock state

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,11 +1,39 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-
+import { render, fireEvent } from '@testing-library/react';
 import Dashboard from './Dashboard';
+import Controls from '../controls/Controls';
 
+// Snapshot test
 describe('<Dashboard/>', () => {
     it('matches snapshot', () => {
         const tree = renderer.create(<Dashboard />);
         expect(tree.toJSON()).toMatchSnapshot();
+    })
+})
+
+// jest.mock('state.locked', () => false)
+// jest.mock('state.closed', () => false)
+
+describe('toggleLocked function', () => {
+    // const spy = jest.spyOn(Dashboard, 'Dashboard.toggleLocked');
+    // console.log(spy)
+    // it('sets state.locked to false if true', () => {
+        
+    // })
+    // it('sets state.locked to true if false', () => {
+
+    // })
+    // it('Is disabled/does nothing if gate is open', () => {
+
+    // })
+})
+
+describe('buttons', () => {
+    it('should show unlock if locked and closed', () => {
+        let lockedMock = true;
+        let closedMock = true;
+        const { getByText, queryByText } = render(<Controls locked={lockedMock} closed={closedMock}/>)
+        expect(queryByText(/unlock/i)).toBeTruthy()
     })
 })
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import renderer from 'react-test-renderer';
 
 import Display from './Display';
 
 describe('<Display/>', () => {
+    // snapshot test
     it('matches snapshot', () => {
         const tree = renderer.create(<Display />);
+        
         expect(tree.toJSON()).toMatchSnapshot();
+    })
+    //
+    it('displays Open when closed = false', () => {
+        let lockedMock = false;
+        let closedMock = false;
+        // const { getByText } = render(<Display locked={lockedMock} closed={closedMock}/>);
     })
 })
